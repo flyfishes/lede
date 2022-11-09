bdc4686c4b1eed29bae4efc93a2931a300cc4d73
allen <34975488+flyfishes@users.noreply.github.com>
2022-11-09 22:46:17 +0800

    Update generic.mk
    restore for ax6
    #https://github.com/flyfishes/lede/edit/master/target/linux/ipq807x/image/generic.mk
    #line 35
    
    define Device/redmi_ax6
            $(call Device/xiaomi_ax3600)
            DEVICE_VENDOR := Redmi
            DEVICE_MODEL := AX6
            DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
    endef
    TARGET_DEVICES += redmi_ax6

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -32,6 +32,14 @@ define Device/qnap_301w
 endef
 TARGET_DEVICES += qnap_301w
 
+define Device/redmi_ax6
+        $(call Device/xiaomi_ax3600)
+        DEVICE_VENDOR := Redmi
+        DEVICE_MODEL := AX6
+        DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
+endef
+TARGET_DEVICES += redmi_ax6
+
 define Device/zte_mf269
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
